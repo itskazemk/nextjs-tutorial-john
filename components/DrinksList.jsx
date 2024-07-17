b6f7f0a55a5ec4db3619837a9c3efda516fea9de
Kazem Kamali <itskazemk@gmail.com>
2024-07-17 13:08:27 +0330

    028 Responsive Images

--- components/DrinksList.jsx
+++ components/DrinksList.jsx
@@ -1,14 +1,24 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const DrinksList = ({ drinks }) => {
   return (
-    <ul className="menu menu-vertical pl-0">
+    <ul className="grid sm:grid-cols-2 gap-6 mt-6">
       {drinks.map((drink) => (
         <li key={drink.idDrink}>
           <Link
             href={`/drinks/${drink.idDrink}`}
             className="text-xl font-medium"
           >
+            <div className="relative h-48 mb-4">
+              <Image
+                src={drink.strDrinkThumb}
+                fill
+                // sizes=""
+                alt={drink.strDrink}
+                className="rounded object-cover"
+              />
+            </div>
             {drink.strDrink}
           </Link>
         </li>
