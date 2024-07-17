b2940c8ee264e7d059ed09b70f34faf961e01eb4
Kazem Kamali <itskazemk@gmail.com>
2024-07-17 09:35:38 +0330

    020 Loading Component

--- app/drinks/page.js
+++ app/drinks/page.js
@@ -1,7 +1,14 @@
 const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a";
 
-const Drinks = async () => {
+const fetchDrinks = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   let data = await fetch(url).then((res) => res.json());
+  return data;
+};
+
+const Drinks = async () => {
+  const data = await fetchDrinks();
+
   console.log(111, data);
 
   return <div>Drinks</div>;
