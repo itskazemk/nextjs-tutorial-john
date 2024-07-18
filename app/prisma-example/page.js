b54049a96dc7f2871b8c8b7562e4de8c0ba9b1db
Kazem Kamali <itskazemk@gmail.com>
2024-07-18 10:24:59 +0330

    032 Prisma CRUD

--- app/prisma-example/page.js
+++ app/prisma-example/page.js
@@ -1,5 +1,32 @@
-const Prisma = () => {
-  return <div>Prisma</div>;
+import prisma from "@/utils/db";
+
+const prismaHandlers = async () => {
+  await prisma.task.create({
+    data: { content: "wake up" },
+  });
+
+  const allTasks = await prisma.task.findMany({
+    orderBy: { createdAt: "desc" },
+  });
+
+  return allTasks;
+};
+
+const Prisma = async () => {
+  const allTasks = await prismaHandlers();
+
+  return (
+    <div>
+      <h2 className="text-5xl font-bold">Prisma</h2>
+      <div>
+        <ul className="">
+          {allTasks.map((task) => (
+            <li key={task.id}>{task.content}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Prisma;
